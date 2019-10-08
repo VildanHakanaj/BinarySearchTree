f7ef1c9a3a89ebb75469f3194cf7c2f29e86d6d4
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2019-10-07 22:50:18 -0400

    - Added the insert method in the binary search tree class -- updated the gitignore file

--- src/com/company/BinarySearchTree.java
+++ src/com/company/BinarySearchTree.java
@@ -2,6 +2,56 @@ package com.company;
 
 public class BinarySearchTree {
 
+  private Node root;
 
+  public BinarySearchTree(){
+    root = null;
+  }
+
+
+  /**
+   * Region Main functions
+   * This contains all the main methods
+   * like Insert(data)
+   *      delete(data)
+   *      inorderTraverse(Node)
+   * */
+
+  public void insert(int data){
+    root = insert(data, root);
+  }
+
+  /**
+   * Method insert
+   * recursively runs down the tree and insert the node accordingly
+   * @param data = data to be inserted
+   * @param root = the root of the current tree
+   * */
+  private Node insert(int data, Node root){
+
+    //Check if the root is null and insert the data
+    if(root == null){
+      return root = new Node(data);
+    }
+
+    //Travers down the tree accordingly if data smaller move left else move right
+    if(root.getData() > data){
+      root.setLeft(insert(data, root.getLeft()));
+    }else{
+      root.setRight(insert(data, root.getRight()));
+    }
+
+    //return the current root;
+    return root;
+
+  }
+
+
+  /*
+  * Region Helper Methods
+  *
+  * This contains helper misc functions used in the algo
+  *
+  * */
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
 
-
-
-
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(9);
+        tree.insert(18);
+        tree.insert(40);
+        tree.insert(30);
+        tree.insert(50);
     }
 }
